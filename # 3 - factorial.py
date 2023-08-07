dab0f9ef2fa01026382a73efe3b2f80680af6641
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-08-07 15:42:36 +0530

    Update # 3 - factorial.py

--- # 3 - factorial.py
+++ # 3 - factorial.py
@@ -1,3 +1,6 @@
+# Program: To get factorial of enterd number.
+# Author: Dev Mehta
+# Date: 7th Aug 2023.
 number = int(input('Enter the number whose factorial you want to find: ' ))
 factorial = 1
 if number < 0 : 
