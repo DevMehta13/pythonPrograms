a31f22fa41b0b1f2b86f5c9ca34ff0c53b631a9c
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-08-07 15:42:09 +0530

    Update # 2 - evenOdd.py

--- # 2 - evenOdd.py
+++ # 2 - evenOdd.py
@@ -1,3 +1,6 @@
+# Program: To check if a number is even or odd.
+# Author: Dev Mehta
+# Date: 7th Aug 2023.
 number = int(input('Enter the number: '))
 if (number % 2) == 0 :
     print('{0} is an even number.' .format(number))
