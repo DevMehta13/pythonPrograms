4fee222485cfe8480b827b8d4be041111f8527a5
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-08-07 21:55:28 +0530

    Add files via upload

---
+++ # 9 - maxMinNumber.py
@@ -0,0 +1,33 @@
+# Program: To find maximum and minimum number in a list.
+# Author: Dev Mehta
+# Date: 7th Aug 2023.
+def swap(x,y) :
+    t = x
+    x = y
+    y = t
+def maximum(list,size) :
+    max = list[0]
+    for i in range (1,(size-1)) :
+        if( list[i] > max) :
+            max = list[i]
+        else :
+            max = max
+    print("The maximum value in list is " , max)
+def minimum(list,size) :
+    min = list[0]
+    for i in range (1,(size-1)) :
+        if( list[i] < min) :
+            min = list[i]
+        else :
+            min = min
+    print("The minimum value in list is " , min)
+def main() :
+    size = int(input("Enter the size of list: "))
+    list = []
+    for i in range (0,(size)) :
+        element = int(input("Enter the element: "))
+        list.append(element)
+    maximum(list,size)
+    minimum(list,size)
+if __name__ == "__main__" :
+    main() 
