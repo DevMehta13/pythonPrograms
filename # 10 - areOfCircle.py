6636c08f89896f06089d53487448e1caf53514e4
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-08-08 22:05:18 +0530

    Add files via upload

---
+++ # 10 - areOfCircle.py
@@ -0,0 +1,6 @@
+# Program: To find area of circle.
+# Author: Dev Mehta
+# Date: 7th Aug 2023.
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14*pow(radius,2)
+print("The area of the circle is " , area)
